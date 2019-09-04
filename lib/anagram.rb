e9d4ef4eae63712deebceb12241b7b6811a8aec7
Caitlin Majewski <cmajewski@wesleyan.edu>
2019-09-04 21:17:00 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,19 @@
+require 'pry'
+class Anagram
+  attr_accessor :word
+  def initialize (word)
+    @word
+  end
+
+  def match (array)
+    array.select do |array_word|
+    array_word.split("").sort
+    end
+    array==@word.split("").sort
+
+
+  end
+
+end
+
 # Your code goes here!
